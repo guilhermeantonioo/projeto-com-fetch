8bb56f12c025e208abe030c118fe5589201a416e
Guilherme Antônio <127310615+guilhermeantonioo@users.noreply.github.com>
2023-07-25 12:39:56 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,92 +1,83 @@
 const url = "https://jsonplaceholder.typicode.com/posts";
 
-const loadingElement = document.querySelector("#loading");
-const postsContainer = document.querySelector("#posts-container");
+const loadingElement    = document.querySelector('#loading');
+const postsContainer    = document.querySelector('#posts-container');
 
-const postPage = document.querySelector("#post");
-const postContainer = document.querySelector("#post-container");
-const commentsContainer = document.querySelector("#comments-container");
+const postPage          = document.querySelector('#post');
+const postContainer     = document.querySelector('#post-container');
+const commentsContainer = document.querySelector('#comments-container')
 
-const commentForm = document.querySelector("#comment-form");
-const emailInput = document.querySelector("#email");
-const bodyInput = document.querySelector("#body");
 
-// Load post
+// get id from URL
 const urlSearchParams = new URLSearchParams(window.location.search);
-const postId = urlSearchParams.get("id");
+const postId          = urlSearchParams.get('id');
 
-// Get all posts
-async function getAllPosts() {
+const commentForm = document.querySelector('#comment-form');
+const emailInput  = document.querySelector('#email');
+const bodyInput   = document.querySelector('#body'); 
+// get all posts
+async function getAllPost() {
   const response = await fetch(url);
-
-  console.log(response);
-
   const data = await response.json();
-
   console.log(data);
-
-  loadingElement.classList.add("hide");
+  loadingElement.classList.add('hide');
 
   data.map((post) => {
-    const div = document.createElement("div");
-    const title = document.createElement("h2");
-    const body = document.createElement("p");
-    const link = document.createElement("a");
+    const div = document.createElement('div');
+    const title = document.createElement('h2');
+    const body = document.createElement('p');
+    const link = document.createElement('a');
 
     title.innerText = post.title;
     body.innerText = post.body;
-    link.innerText = "Ler";
+    link.innerText = 'Ler';
     link.setAttribute("href", `./post.html?id=${post.id}`);
 
     div.appendChild(title);
     div.appendChild(body);
     div.appendChild(link);
+
     postsContainer.appendChild(div);
-  });
+
+  })
 }
 
-// Get individual post
-async function getPost(id) {
-  const [responsePost, responseComments] = await Promise.all([
-    fetch(`${url}/${id}`),
-    fetch(`${url}/${id}/comments`),
-  ]);
+// get individual post 
+async function getPost(id){
 
+  const [responsePost , responseComments] = await Promise.all([
+
+    fetch(`${url}/${id}`), 
+    fetch(`${url}/${id}/comments`)
+  ])
   const dataPost = await responsePost.json();
-
   const dataComments = await responseComments.json();
 
-  loadingElement.classList.add("hide");
-  postPage.classList.remove("hide");
+  loadingElement.classList.add('hide');
+  postPage.classList.remove('hide');
 
-  const title = document.createElement("h1");
-  const body = document.createElement("p");
+  const title = document.createElement('h1')
+  const body = document.createElement('p')
 
   title.innerText = dataPost.title;
-  body.innerText = dataPost.body;
+  body.innerText  = dataPost.body;
 
-  postContainer.appendChild(title);
-  postContainer.appendChild(body);
 
+  postContainer.appendChild(title)
+  postContainer.appendChild(body)
+
+  console.log(dataComments)
   dataComments.map((comment) => {
-    createComment(comment);
-  });
+    createComment(comment)
+  })
 }
 
-function createComment(comment) {
-  const div = document.createElement("div");
-  const email = document.createElement("h3");
-  const commentBody = document.createElement("p");
+// Post a comment 
 
-  email.innerText = comment.email;
-  commentBody.innerText = comment.body;
 
-  div.appendChild(email);
-  div.appendChild(commentBody);
-  commentsContainer.appendChild(div);
-}
+  // POST, PUT, PATCH, DELETE - utiliza-se os headers
 
-// Insert a comment
+
 async function postComment(comment) {
   const response = await fetch(url, {
     method: "POST",
@@ -98,28 +89,40 @@ async function postComment(comment) {
 
   const data = await response.json();
 
+  console.log(data)
   createComment(data);
 }
 
+function createComment(comment){
+  const div = document.createElement('div')
+  const email = document.createElement('h3')
+  const commentBody = document.createElement('p')
+
+  email.innerText = comment.email;
+  commentBody.innerText = comment.body;
+
+  div.appendChild(email);
+  div.appendChild(commentBody);
+
+  commentsContainer.appendChild(div);
+}
 if (!postId) {
-  getAllPosts();
+  getAllPost()
 } else {
   getPost(postId);
 
-  commentForm.addEventListener("submit", (e) => {
+  // add event from comment form
+
+  commentForm.addEventListener('submit' , (e)=>{
     e.preventDefault();
 
-    let comment = {
-      email: emailInput.value,
+    let commments = {
+      email: emailInput.value, 
       body: bodyInput.value,
+
     };
-
-    comment = JSON.stringify(comment);
-
-    postComment(comment);
-  });
+    commments = JSON.stringify(commments)
+    postComment(commments)
+  })
 }
 
-    postComment(comment);
-  });
-}
